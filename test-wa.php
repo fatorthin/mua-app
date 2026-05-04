96ea5fea770b234e53bbf3fd0bd4c2e48b18f6c1
Fathin Mubarak <fathin.mubarak@gmail.com>
2026-05-04 14:27:48 +0700

    update

--- test-wa.php
+++
@@ -1,5 +0,0 @@
-<?php
-$response = file_get_contents("https://wagateway.surakana.my.id/docs/swagger.json", false, stream_context_create([
-    "http" => ["header" => "Authorization: Basic " . base64_encode("admin:admin")]
-]));
-echo strip_tags($response);
